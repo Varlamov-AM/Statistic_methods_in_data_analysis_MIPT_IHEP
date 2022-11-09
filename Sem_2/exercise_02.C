8a7cc83b0cf32d72ebe276031bb7b56d54febf2b
Varlamov Antony <Varlamov.AM@phystech.edu>
2022-11-09 10:25:01 +0300

    Added materials from 2 seminar

---
+++ Sem_2/exercise_02.C
@@ -0,0 +1,98 @@
+
+#include "RooRealVar.h"
+#include "RooDataSet.h"
+#include "RooGaussian.h"
+#include "TCanvas.h"
+#include "RooPlot.h"
+#include "TAxis.h"
+#include "TMath.h"
+using namespace RooFit;
+
+Double_t DoubleGauss(Double_t x, Double_t mu1, Double_t sigma1){//,
+   //Double_t mu2, Double_t sigma2){
+      Double_t result = TMath::Gaus(x, mu1, sigma1) + TMath::Gaus(x, mu1, sigma1);
+      return result;
+   }
+
+void exercise_02()
+{
+   // S e t u p   m o d e l
+   // ---------------------
+
+   // Declare variables x,mean,sigma with associated name, title, initial value and allowed range
+   RooRealVar x("x", "x", -10, 10);
+   RooRealVar mean1("mean", "mean of gaussian", -3, -10, 10);
+   RooRealVar sigma1("sigma", "width of gaussian", 2, 0.1, 10);
+   RooRealVar mean2("mean", "mean of gaussian", 3, -10, 10);
+   RooRealVar sigma2("sigma", "width of gaussian", 2, 0.1, 10);
+
+   // Build gaussian pdf in terms of x,mean and sigma
+   RooGaussian gauss1("gauss1", "gaussian1 PDF", x, mean1, sigma1);
+   RooGaussian gauss2("gauss2", "gaussian2 PDF", x, mean2, sigma2);
+
+   RooRealVar nsig("nsig","#signal events",800,0.,10000) ;
+   RooRealVar nbkg("nbkg","#background events",800,0.,10000) ;
+
+   RooAddPdf Double_Gaus("Double_Gaus", "Gaus + Gaus", RooArgList(gauss1, gauss2), RooArgList(nsig,nbkg));
+
+
+   // Construct plot frame in 'x'
+   RooPlot *xframe = x.frame(Title("Gaussian pdf."));
+
+   // P l o t   m o d e l   a n d   c h a n g e   p a r a m e t e r   v a l u e s
+   // ---------------------------------------------------------------------------
+
+   // Plot gauss in frame (i.e. in x)
+   gauss1.plotOn(xframe);
+   gauss2.plotOn(xframe);  
+   // Change the value of sigma to 3
+   //sigma1.setVal(3);
+
+   // Plot gauss in frame (i.e. in x) and draw frame on canvas
+   //gauss1.plotOn(xframe, LineColor(kRed));
+
+   // G e n e r a t e   e v e n t s
+   // -----------------------------
+
+   // Generate a dataset of 1000 events in x from gauss
+   RooDataSet *data1 = gauss1.generate(x, 100000);
+   RooDataSet *data2 = gauss2.generate(x, 150000);
+   RooDataSet *data3 = Double_Gaus.generate(x, 100000);
+   
+   data2->append((*data1));
+   
+
+   // Make a second plot frame in x and draw both the
+   // data and the pdf in the frame
+   RooPlot *xframe2 = x.frame(Title("Gaussian pdf with data"));
+   data2->plotOn(xframe2);
+
+
+   RooPlot *xframe3 = x.frame(Title("2 Gaussian pdf with data"));
+   //data3->plotOn(xframe3);
+   Double_Gaus.plotOn(xframe3);
+   
+   //gauss1.plotOn(xframe2);
+
+   // F i t   m o d e l   t o   d a t a
+   // -----------------------------
+
+   // Fit pdf to data
+   // gauss.fitTo(*data);
+
+   // Print values of mean and sigma (that now reflect fitted values and errors)
+   // mean.Print();
+   // sigma.Print();
+
+   // Draw all frames on a canvas
+   TCanvas *c = new TCanvas("rf101_basics", "rf101_basics", 800, 400);
+   c->Divide(2);
+   c->cd(1);
+   gPad->SetLeftMargin(0.15);
+   xframe3->GetYaxis()->SetTitleOffset(1.6);
+   xframe3->Draw();
+   c->cd(2);
+   gPad->SetLeftMargin(0.15);
+   xframe2->GetYaxis()->SetTitleOffset(1.6);
+   xframe2->Draw();
+}
