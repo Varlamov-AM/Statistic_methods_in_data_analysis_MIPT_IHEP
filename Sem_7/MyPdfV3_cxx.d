0f0467dbbadd4a48ea5a37dd36b1d4dc94ef6f75
Varlamov Antony <Varlamov.AM@phystech.edu>
2022-11-09 10:25:40 +0300

    Added materials from 7 seminar

---
+++ Sem_7/MyPdfV3_cxx.d
@@ -0,0 +1,82 @@
+
+# DO NOT DELETE
+
+./MyPdfV3_cxx.so: /usr/local/include/Riostream.h MyPdfV3.h
+./MyPdfV3_cxx.so: /usr/local/include/RooAbsPdf.h
+./MyPdfV3_cxx.so: /usr/local/include/RooAbsReal.h
+./MyPdfV3_cxx.so: /usr/local/include/RooAbsArg.h /usr/local/include/TNamed.h
+./MyPdfV3_cxx.so: /usr/local/include/TObject.h /usr/local/include/Rtypes.h
+./MyPdfV3_cxx.so: /usr/local/include/RtypesCore.h
+./MyPdfV3_cxx.so: /usr/local/include/ROOT/RConfig.hxx
+./MyPdfV3_cxx.so: /usr/local/include/RVersion.h
+./MyPdfV3_cxx.so: /usr/local/include/RConfigure.h
+./MyPdfV3_cxx.so: /usr/local/include/DllImport.h /usr/local/include/strtok.h
+./MyPdfV3_cxx.so: /usr/local/include/strlcpy.h /usr/local/include/snprintf.h
+./MyPdfV3_cxx.so: /usr/local/include/TGenericClassInfo.h
+./MyPdfV3_cxx.so: /usr/local/include/TSchemaHelper.h
+./MyPdfV3_cxx.so: /usr/local/include/TIsAProxy.h
+./MyPdfV3_cxx.so: /usr/local/include/TVirtualIsAProxy.h
+./MyPdfV3_cxx.so: /usr/local/include/TStorage.h
+./MyPdfV3_cxx.so: /usr/local/include/TVersionCheck.h
+./MyPdfV3_cxx.so: /usr/local/include/RVersion.h /usr/local/include/TString.h
+./MyPdfV3_cxx.so: /usr/local/include/TMathBase.h
+./MyPdfV3_cxx.so: /usr/local/include/ROOT/RStringView.hxx
+./MyPdfV3_cxx.so: /usr/local/include/ROOT/TypeTraits.hxx
+./MyPdfV3_cxx.so: /usr/local/include/TObjArray.h
+./MyPdfV3_cxx.so: /usr/local/include/TSeqCollection.h
+./MyPdfV3_cxx.so: /usr/local/include/TCollection.h
+./MyPdfV3_cxx.so: /usr/local/include/TIterator.h
+./MyPdfV3_cxx.so: /usr/local/include/TVirtualRWMutex.h
+./MyPdfV3_cxx.so: /usr/local/include/TVirtualMutex.h
+./MyPdfV3_cxx.so: /usr/local/include/ROOT/RRangeCast.hxx
+./MyPdfV3_cxx.so: /usr/local/include/ROOT/RSpan.hxx
+./MyPdfV3_cxx.so: /usr/local/include/ROOT/span.hxx
+./MyPdfV3_cxx.so: /usr/local/include/TRefArray.h
+./MyPdfV3_cxx.so: /usr/local/include/TProcessID.h
+./MyPdfV3_cxx.so: /usr/local/include/RooPrintable.h
+./MyPdfV3_cxx.so: /usr/local/include/RooSTLRefCountList.h
+./MyPdfV3_cxx.so: /usr/local/include/RooNameReg.h
+./MyPdfV3_cxx.so: /usr/local/include/RooAbsCache.h
+./MyPdfV3_cxx.so: /usr/local/include/RooLinkedListIter.h
+./MyPdfV3_cxx.so: /usr/local/include/RooLinkedList.h
+./MyPdfV3_cxx.so: /usr/local/include/RooLinkedListElem.h
+./MyPdfV3_cxx.so: /usr/local/include/RooStringView.h
+./MyPdfV3_cxx.so: /usr/local/include/RooCmdArg.h
+./MyPdfV3_cxx.so: /usr/local/include/RooCurve.h /usr/local/include/TGraph.h
+./MyPdfV3_cxx.so: /usr/local/include/TAttLine.h /usr/local/include/TAttFill.h
+./MyPdfV3_cxx.so: /usr/local/include/TAttMarker.h
+./MyPdfV3_cxx.so: /usr/local/include/TVectorFfwd.h
+./MyPdfV3_cxx.so: /usr/local/include/TVectorDfwd.h
+./MyPdfV3_cxx.so: /usr/local/include/TFitResultPtr.h
+./MyPdfV3_cxx.so: /usr/local/include/RooPlotable.h
+./MyPdfV3_cxx.so: /usr/local/include/TMatrixDfwd.h
+./MyPdfV3_cxx.so: /usr/local/include/RooArgSet.h
+./MyPdfV3_cxx.so: /usr/local/include/RooAbsCollection.h
+./MyPdfV3_cxx.so: /usr/local/include/RooFit/UniqueId.h
+./MyPdfV3_cxx.so: /usr/local/include/RooArgList.h
+./MyPdfV3_cxx.so: /usr/local/include/RooGlobalFunc.h
+./MyPdfV3_cxx.so: /usr/local/include/RooSpan.h
+./MyPdfV3_cxx.so: /usr/local/include/RooBatchComputeTypes.h
+./MyPdfV3_cxx.so: /usr/local/include/RooFit/Detail/DataMap.h
+./MyPdfV3_cxx.so: /usr/local/include/RooObjCacheManager.h
+./MyPdfV3_cxx.so: /usr/local/include/RooNormSetCache.h
+./MyPdfV3_cxx.so: /usr/local/include/RooAbsCacheElement.h
+./MyPdfV3_cxx.so: /usr/local/include/RooCacheManager.h
+./MyPdfV3_cxx.so: /usr/local/include/RooMsgService.h
+./MyPdfV3_cxx.so: /usr/local/include/RooHelpers.h
+./MyPdfV3_cxx.so: /usr/local/include/RooRealProxy.h
+./MyPdfV3_cxx.so: /usr/local/include/RooTemplateProxy.h
+./MyPdfV3_cxx.so: /usr/local/include/RooArgProxy.h
+./MyPdfV3_cxx.so: /usr/local/include/RooAbsProxy.h
+./MyPdfV3_cxx.so: /usr/local/include/RooAbsRealLValue.h
+./MyPdfV3_cxx.so: /usr/local/include/RooNumber.h
+./MyPdfV3_cxx.so: /usr/local/include/RooAbsLValue.h
+./MyPdfV3_cxx.so: /usr/local/include/RooAbsBinning.h
+./MyPdfV3_cxx.so: /usr/local/include/RooAbsCategory.h
+./MyPdfV3_cxx.so: /usr/local/include/RooCatType.h
+./MyPdfV3_cxx.so: /usr/local/include/RooFitLegacy/RooCatTypeLegacy.h
+./MyPdfV3_cxx.so: /usr/local/include/RooCategoryProxy.h
+./MyPdfV3_cxx.so: /usr/local/include/RooAbsCategoryLValue.h
+./MyPdfV3_cxx.so: /usr/local/include/TMath.h /usr/local/include/TError.h
+./MyPdfV3_cxx.so: /usr/local/include/RVersion.h /usr/local/include/ROOT/RConfig.hxx /usr/local/include/TClass.h /usr/local/include/TDictAttributeMap.h /usr/local/include/TInterpreter.h /usr/local/include/TROOT.h /usr/local/include/TBuffer.h /usr/local/include/TMemberInspector.h /usr/local/include/TError.h /usr/local/include/RtypesImp.h /usr/local/include/TIsAProxy.h /usr/local/include/TFileMergeInfo.h /usr/local/include/TCollectionProxyInfo.h /usr/local/bin/rootcling
+MyPdfV3_cxx__ROOTBUILDVERSION= 6.27/01
---
+++ Sem_7/RooGenPdfPdf_cxx.d
@@ -0,0 +1,87 @@
+
+# DO NOT DELETE
+
+RooGenPdfPdf_cxx.so: /usr/local/include/Riostream.h RooGenPdfPdf.h
+RooGenPdfPdf_cxx.so: /usr/local/include/RooAbsPdf.h
+RooGenPdfPdf_cxx.so: /usr/local/include/RooAbsReal.h
+RooGenPdfPdf_cxx.so: /usr/local/include/RooAbsArg.h
+RooGenPdfPdf_cxx.so: /usr/local/include/TNamed.h /usr/local/include/TObject.h
+RooGenPdfPdf_cxx.so: /usr/local/include/Rtypes.h
+RooGenPdfPdf_cxx.so: /usr/local/include/RtypesCore.h
+RooGenPdfPdf_cxx.so: /usr/local/include/ROOT/RConfig.hxx
+RooGenPdfPdf_cxx.so: /usr/local/include/RVersion.h
+RooGenPdfPdf_cxx.so: /usr/local/include/RConfigure.h
+RooGenPdfPdf_cxx.so: /usr/local/include/DllImport.h
+RooGenPdfPdf_cxx.so: /usr/local/include/strtok.h /usr/local/include/strlcpy.h
+RooGenPdfPdf_cxx.so: /usr/local/include/snprintf.h
+RooGenPdfPdf_cxx.so: /usr/local/include/TGenericClassInfo.h
+RooGenPdfPdf_cxx.so: /usr/local/include/TSchemaHelper.h
+RooGenPdfPdf_cxx.so: /usr/local/include/TIsAProxy.h
+RooGenPdfPdf_cxx.so: /usr/local/include/TVirtualIsAProxy.h
+RooGenPdfPdf_cxx.so: /usr/local/include/TStorage.h
+RooGenPdfPdf_cxx.so: /usr/local/include/TVersionCheck.h
+RooGenPdfPdf_cxx.so: /usr/local/include/RVersion.h
+RooGenPdfPdf_cxx.so: /usr/local/include/TString.h
+RooGenPdfPdf_cxx.so: /usr/local/include/TMathBase.h
+RooGenPdfPdf_cxx.so: /usr/local/include/ROOT/RStringView.hxx
+RooGenPdfPdf_cxx.so: /usr/local/include/ROOT/TypeTraits.hxx
+RooGenPdfPdf_cxx.so: /usr/local/include/TObjArray.h
+RooGenPdfPdf_cxx.so: /usr/local/include/TSeqCollection.h
+RooGenPdfPdf_cxx.so: /usr/local/include/TCollection.h
+RooGenPdfPdf_cxx.so: /usr/local/include/TIterator.h
+RooGenPdfPdf_cxx.so: /usr/local/include/TVirtualRWMutex.h
+RooGenPdfPdf_cxx.so: /usr/local/include/TVirtualMutex.h
+RooGenPdfPdf_cxx.so: /usr/local/include/ROOT/RRangeCast.hxx
+RooGenPdfPdf_cxx.so: /usr/local/include/ROOT/RSpan.hxx
+RooGenPdfPdf_cxx.so: /usr/local/include/ROOT/span.hxx
+RooGenPdfPdf_cxx.so: /usr/local/include/TRefArray.h
+RooGenPdfPdf_cxx.so: /usr/local/include/TProcessID.h
+RooGenPdfPdf_cxx.so: /usr/local/include/RooPrintable.h
+RooGenPdfPdf_cxx.so: /usr/local/include/RooSTLRefCountList.h
+RooGenPdfPdf_cxx.so: /usr/local/include/RooNameReg.h
+RooGenPdfPdf_cxx.so: /usr/local/include/RooAbsCache.h
+RooGenPdfPdf_cxx.so: /usr/local/include/RooLinkedListIter.h
+RooGenPdfPdf_cxx.so: /usr/local/include/RooLinkedList.h
+RooGenPdfPdf_cxx.so: /usr/local/include/RooLinkedListElem.h
+RooGenPdfPdf_cxx.so: /usr/local/include/RooStringView.h
+RooGenPdfPdf_cxx.so: /usr/local/include/RooCmdArg.h
+RooGenPdfPdf_cxx.so: /usr/local/include/RooCurve.h
+RooGenPdfPdf_cxx.so: /usr/local/include/TGraph.h
+RooGenPdfPdf_cxx.so: /usr/local/include/TAttLine.h
+RooGenPdfPdf_cxx.so: /usr/local/include/TAttFill.h
+RooGenPdfPdf_cxx.so: /usr/local/include/TAttMarker.h
+RooGenPdfPdf_cxx.so: /usr/local/include/TVectorFfwd.h
+RooGenPdfPdf_cxx.so: /usr/local/include/TVectorDfwd.h
+RooGenPdfPdf_cxx.so: /usr/local/include/TFitResultPtr.h
+RooGenPdfPdf_cxx.so: /usr/local/include/RooPlotable.h
+RooGenPdfPdf_cxx.so: /usr/local/include/TMatrixDfwd.h
+RooGenPdfPdf_cxx.so: /usr/local/include/RooArgSet.h
+RooGenPdfPdf_cxx.so: /usr/local/include/RooAbsCollection.h
+RooGenPdfPdf_cxx.so: /usr/local/include/RooFit/UniqueId.h
+RooGenPdfPdf_cxx.so: /usr/local/include/RooArgList.h
+RooGenPdfPdf_cxx.so: /usr/local/include/RooGlobalFunc.h
+RooGenPdfPdf_cxx.so: /usr/local/include/RooSpan.h
+RooGenPdfPdf_cxx.so: /usr/local/include/RooBatchComputeTypes.h
+RooGenPdfPdf_cxx.so: /usr/local/include/RooFit/Detail/DataMap.h
+RooGenPdfPdf_cxx.so: /usr/local/include/RooObjCacheManager.h
+RooGenPdfPdf_cxx.so: /usr/local/include/RooNormSetCache.h
+RooGenPdfPdf_cxx.so: /usr/local/include/RooAbsCacheElement.h
+RooGenPdfPdf_cxx.so: /usr/local/include/RooCacheManager.h
+RooGenPdfPdf_cxx.so: /usr/local/include/RooMsgService.h
+RooGenPdfPdf_cxx.so: /usr/local/include/RooHelpers.h
+RooGenPdfPdf_cxx.so: /usr/local/include/RooRealProxy.h
+RooGenPdfPdf_cxx.so: /usr/local/include/RooTemplateProxy.h
+RooGenPdfPdf_cxx.so: /usr/local/include/RooArgProxy.h
+RooGenPdfPdf_cxx.so: /usr/local/include/RooAbsProxy.h
+RooGenPdfPdf_cxx.so: /usr/local/include/RooAbsRealLValue.h
+RooGenPdfPdf_cxx.so: /usr/local/include/RooNumber.h
+RooGenPdfPdf_cxx.so: /usr/local/include/RooAbsLValue.h
+RooGenPdfPdf_cxx.so: /usr/local/include/RooAbsBinning.h
+RooGenPdfPdf_cxx.so: /usr/local/include/RooAbsCategory.h
+RooGenPdfPdf_cxx.so: /usr/local/include/RooCatType.h
+RooGenPdfPdf_cxx.so: /usr/local/include/RooFitLegacy/RooCatTypeLegacy.h
+RooGenPdfPdf_cxx.so: /usr/local/include/RooCategoryProxy.h
+RooGenPdfPdf_cxx.so: /usr/local/include/RooAbsCategoryLValue.h
+RooGenPdfPdf_cxx.so: /usr/local/include/TMath.h /usr/local/include/TError.h
+RooGenPdfPdf_cxx.so: /usr/local/include/RVersion.h /usr/local/include/ROOT/RConfig.hxx /usr/local/include/TClass.h /usr/local/include/TDictAttributeMap.h /usr/local/include/TInterpreter.h /usr/local/include/TROOT.h /usr/local/include/TBuffer.h /usr/local/include/TMemberInspector.h /usr/local/include/TError.h /usr/local/include/RtypesImp.h /usr/local/include/TIsAProxy.h /usr/local/include/TFileMergeInfo.h /usr/local/include/TCollectionProxyInfo.h /usr/local/bin/rootcling
+RooGenPdfPdf_cxx__ROOTBUILDVERSION= 6.27/01
